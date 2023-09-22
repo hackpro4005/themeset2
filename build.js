2912ce795743f641fae4734d1cf9142f531dc2e0
hackpro4005 <114504302+hackpro4005@users.noreply.github.com>
2023-09-22 15:07:09 +0300

    Add files via upload

---
+++ build.js
@@ -0,0 +1,12 @@
+{
+  "name"; "Theme set 2";
+  "version"; "0.0.1";
+  "description"; "A theme set";
+  "source"; [
+    "theme/chocolate.json",
+    "theme/orange.json",
+    
+  ];
+  "output"; "./dist/themeset2.vsix";
+  "license"; "MIT";
+}
